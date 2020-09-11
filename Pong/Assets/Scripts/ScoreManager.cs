2b5a4f264f0c54d789492fd0a8f5a95e3bfb3dab
fjhernan <60201738+fjhernan@users.noreply.github.com>
2020-09-10 19:21:31 -0700

    Revert "Pong Project Part 1A edits"
    This reverts commit 82336f95b2274a72c3009d4f8d462055146d2fd6.

--- Pong/Assets/Scripts/ScoreManager.cs
+++ Pong/Assets/Scripts/ScoreManager.cs
@@ -20,26 +20,24 @@ public class ScoreManager : MonoBehaviour
         if (lGoal.GetComponent<LeftGoal>().score != left_score)
         {
             left_score = lGoal.GetComponent<LeftGoal>().score;
-            Debug.Log("Right Player Scores!\nL: " + left_score + "   R: " + right_score);
+            Debug.Log("Left Player Scores!\nL: " + left_score + "   R: " + right_score);
         }
         if (rGoal.GetComponent<RightGoal>().score != right_score)
         {
             right_score = rGoal.GetComponent<RightGoal>().score;
-            Debug.Log("Left Player Scores!\nL: " + left_score + "   R: " + right_score);
+            Debug.Log("Right Player Scores!\nL: " + left_score + "   R: " + right_score);
         }
 
         if (lGoal.GetComponent<LeftGoal>().score == 11)
         {
-            Debug.Log("Right Player Win!");
+            Debug.Log("Left Player Win!");
             lGoal.GetComponent<LeftGoal>().score = 0;
-            rGoal.GetComponent<RightGoal>().score = 0;
             left_score = 0;
             right_score = 0;
         }
         if (rGoal.GetComponent<RightGoal>().score == 11)
         {
-            Debug.Log("Left Player Wins!");
-            lGoal.GetComponent<LeftGoal>().score = 0;
+            Debug.Log("Right Player Wins!");
             rGoal.GetComponent<RightGoal>().score = 0;
             left_score = 0;
             right_score = 0;
